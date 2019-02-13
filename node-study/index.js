1ca8dd24179009cb81f4a772a75eea4f933dda5e
lihao <lihao.cuapp.me>
2019-02-13 11:24:18 +0800

    学习

--- node-study/index.js
+++
@@ -1,8 +0,0 @@
-var url = require('url')
-var a = url.resolve('/one/two/three/', '/four');         // '/one/two/four'
-var b = url.resolve('http://example.com', 'one');    // 'http://example.com/one'
-var c = url.resolve('http://example.com/one/', 'two'); // 'http://example.com/two'
-console.log(a)
-console.log(b)
-console.log(c)
-
--- node-study/lesson4.js
+++ node-study/lesson4.js
@@ -16,21 +16,54 @@ var cnodeUrl = 'https://cnodejs.org'
 
 app.get('/', function (req, res) {
     superagent.get(cnodeUrl)
-        .end(function (err, _res) {
+        .end(function (err, sres) {
             if (err) {
                 throw new error(err)
             }
             var arr = []
-            var $ = cheerio.load(_res.text)
+            var $ = cheerio.load(sres.text)
             $('#topic_list .cell').each(function (i, item) {
                 //url.resolve  不懂....
                 // var href = url.resolve(cnodeUrl, $(item).find('.topic_title').attr('href')
                 var href = cnodeUrl + $(item).find('.topic_title').attr('href')
                 arr.push(href)
             });
-            res.send(arr)
+
+            // 得到一个 eventproxy 的实例
+            var ep = new eventproxy();
+
+            // 命令 ep 重复监听 arr.length 次（在这里也就是 40 次） `topic_html` 事件再行动
+            ep.after('topic_html', 1, function (topics) {
+                // topics 不是arr，是包含了 arr.length 次 ep.emit('topic_html', pair) 中的那 40 个 值
+                // 开始行动   map 类似与each
+
+                topics = topics.map(function (topicPair) {
+                    // 接下来都是 jquery 的用法了
+                    var topicUrl = topicPair[0];   //传来的url
+                    var topicHtml = topicPair[1];  //传来的html
+                    var $ = cheerio.load(topicHtml);
+                    return ({
+                        title: $('.topic_full_title').text().trim(),
+                        href: topicUrl,
+                        comment1: $('.reply_content').eq(0).text().trim(),
+                    });
+                });
+                res.send(topics)
+                // console.log('final:');
+                // console.log(topics);
+            });
+            //重复5次发送请求,获取原始内容(html)
+
+            arr.slice(0, 1).forEach(function (topicUrl) {
+                superagent.get(topicUrl)
+                    .end(function (err, res) {
+                        // console.log('fetch ' + topicUrl + ' successful');
+                        ep.emit('topic_html', [topicUrl, res.text]);
+                    });
+            });
         })
 });
+
 app.listen(3000, function () {
     console.log('success')
 })
